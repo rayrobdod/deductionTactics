7666f59462f40432703a411afe6bc0d280be89e1
Raymond Dodge <git@rayrobdod.name>
2015-07-06 14:18:38 -0400

    Changing header to say "(C) 2015"

--- src/main/scala/com/rayrobdod/deductionTactics/consoleView/BoardPrinter.scala
+++ src/main/scala/com/rayrobdod/deductionTactics/consoleView/BoardPrinter.scala
@@ -1,7 +1,7 @@
 package com.rayrobdod.deductionTactics
 /*
 	Deduction Tactics
-	Copyright (C) 2012-2013  Raymond Dodge
+	Copyright (C) 2012-2015  Raymond Dodge
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
--- src/main/scala/com/rayrobdod/deductionTactics/consoleView/TokenSelector.scala
+++ src/main/scala/com/rayrobdod/deductionTactics/consoleView/TokenSelector.scala
@@ -1,6 +1,6 @@
 /*
 	Deduction Tactics
-	Copyright (C) 2014  Raymond Dodge
+	Copyright (C) 2012-2015  Raymond Dodge
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
